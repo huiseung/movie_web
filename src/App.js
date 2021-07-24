8318740b49bcc3b6ab542f6ff5443071f10fb07e
huiseung <whtjddn75@gmail.com>
2021-07-25 04:34:59 +0900

    show top20 movie title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component{
+  state ={
+    isLoading: true,
+    movie_list: []
+  };
+
+  getMovies = async ()=>{
+    const {
+      data:{
+        data:{
+          movies
+        }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    //const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    //console.log(movies);
+    this.setState({movie_list: movies, isLoading: false});
+  };
+
+  componentDidMount(){
+    this.getMovies();
+  };
+
+  render(){
+    const {isLoading, movie_list} = this.state;
+    return (
+    <div>{isLoading ? 
+      "Loading...": 
+      movie_list.map(movie =>{
+        console.log(movie); 
+        return <Movie id={movie.id} year={movie.year} title={movie.title} sumary={movie.sumary} poster={movie.medium_cover_image}/>
+        }
+        )
+      }
     </div>
-  );
+    );
+  }
 }
 
 export default App;
